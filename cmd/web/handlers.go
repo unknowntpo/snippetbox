9dd8ba6553f647030f8d4ecad4fecc1b864e094a
Chang Chen Chien <e850506@gmail.com>
2021-01-25 09:46:38 +0800

    Chapter 3.4. Centralized Error Handling: with wrong error line number and file
    Error occured at helpers.go:13, not what we want.
    
    $ go run ./cmd/web
    INFO	2021/01/25 09:48:45 Starting server on :4000
    ERROR	2021/01/25 09:48:47 helpers.go:13: open ../../ui/html/home.page.tmpl: no such file or directory
    goroutine 19 [running]:
    runtime/debug.Stack(0xc00011d200, 0xc000130280, 0x3c)
    	/usr/local/Cellar/go/1.15.6/libexec/src/runtime/debug/stack.go:24 +0x9f
    main.(*application).serverError(0xc00011ad30, 0x142bf20, 0xc0001760e0, 0x1426780, 0xc00011d200)
    	/Users/unknowntpo/repo/snippetbox/cmd/web/helpers.go:12 +0x65
    main.(*application).home(0xc00011ad30, 0x142bf20, 0xc0001760e0, 0xc000196000)
    	/Users/unknowntpo/repo/snippetbox/cmd/web/handlers.go:28 +0x225
    net/http.HandlerFunc.ServeHTTP(0xc00011ad40, 0x142bf20, 0xc0001760e0, 0xc000196000)
    	/usr/local/Cellar/go/1.15.6/libexec/src/net/http/server.go:2042 +0x44
    net/http.(*ServeMux).ServeHTTP(0xc00013c300, 0x142bf20, 0xc0001760e0, 0xc000196000)
    	/usr/local/Cellar/go/1.15.6/libexec/src/net/http/server.go:2417 +0x1ad
    net/http.serverHandler.ServeHTTP(0xc000176000, 0x142bf20, 0xc0001760e0, 0xc000196000)
    	/usr/local/Cellar/go/1.15.6/libexec/src/net/http/server.go:2843 +0xa3
    net/http.(*conn).serve(0xc00013b0e0, 0x142c8a0, 0xc00013c3c0)
    	/usr/local/Cellar/go/1.15.6/libexec/src/net/http/server.go:1925 +0x8ad
    created by net/http.(*Server).Serve
    	/usr/local/Cellar/go/1.15.6/libexec/src/net/http/server.go:2969 +0x36c

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,7 +11,7 @@ import (
 // "Hello from Snippetbox" as the response body.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -22,15 +22,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 		return
 	}
 }
@@ -40,7 +38,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -52,7 +50,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", 405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
