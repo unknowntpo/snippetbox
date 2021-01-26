4d5a7cf8c5748b535e938c4a4d0cf94acba770d1
Chang Chen Chien <e850506@gmail.com>
2021-01-26 09:27:27 +0800

    Chapter 3.5. Isolating the Application Routes
    Create func (app *application) routes() *http.ServeMux {}
    at cmd/ewb/routes.go to tidy up functions in main()

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -30,19 +30,11 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	// Init a server, and set customized logger for server
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
