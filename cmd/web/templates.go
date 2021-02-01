900a5f6db75a0d13bb8f1fe574d362a73bc428f6
Chang Chen Chien <e850506@gmail.com>
2021-02-01 09:00:28 +0800

    Chapter 5.6. Custom Template Functions
    Create custom humanDate() function to show the date.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/unknowntpo/snippetbox/pkg/models"
 )
@@ -15,6 +16,19 @@ type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -31,12 +45,9 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 
 	// Loop through the pages one-by-one.
 	for _, page := range pages {
-		// Extract the file name (like 'home.page.tmpl') from the full file path
-		// and assign it to the name variable.
 		name := filepath.Base(page)
 
-		// Parse the page template file in to a template set.
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
