f2c009602e69661b6c71012a86496aed6e10f917
Chang Chen Chien <e850506@gmail.com>
2021-02-02 10:12:15 +0800

    Chapter 8.3. Data Validation
    In this commit, we:
        - Check that the title, content and expires fields are not empty.
        - Check that the title field is not more than 100 characters long.
        - Check that the expires value matches one of our permitted values (1, 7 or 365 days).
        - And display the error message when user post an invalid form.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/unknowntpo/snippetbox/pkg/models"
 )
@@ -55,23 +57,46 @@ func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request
 // Handle path /snippet/create
 // Action: create new snippet
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	// First we call r.ParseForm() which adds any data in POST request bodies
-	// to the r.PostForm map. This also works in the same way for PUT and PATCH
-	// requests. If there are any errors, we use our app.ClientError helper to send
-	// a 400 Bad Request response to the user.
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	// Use the r.PostForm.Get() method to retrieve the relevant data fields
-	// from the r.PostForm map.
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
 	expires := r.PostForm.Get("expires")
 
-	// Create a new snippet record in the database using the form data.
+	// Initialize a map to hold any validation errors.
+	errors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		errors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		errors["title"] = "This field is too long (maximum is 100 characters)"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		errors["content"] = "This field cannot be blank"
+	}
+
+	if strings.TrimSpace(expires) == "" {
+		errors["expires"] = "This field cannot be blank"
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		errors["expires"] = "This field is invalid"
+	}
+
+	// If there are any validation errors, re-display the create.page.tmpl
+	// template passing in the validation errors and previously submitted
+	// r.PostForm data.
+	if len(errors) > 0 {
+		app.render(w, r, "create.page.tmpl", &templateData{
+			FormErrors: errors,
+			FormData:   r.PostForm,
+		})
+		return
+	}
+
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
