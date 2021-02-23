7260d2ceb5e17eefb1edcfd9272279081ae969a7
Chang Chen Chien <e850506@gmail.com>
2021-02-23 09:14:26 +0800

    Chapter 11.3. User Signup and Password Encryption Storing the User Details

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -123,9 +123,25 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
 		return
 	}
+	// Try to create a new user record in the database. If the email already exists
+	// add an error message to the form and re-display it.
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.Errors.Add("email", "Address is already in use")
+			app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
 
-	// Otherwise send a placeholder response (for now!).
-	fmt.Fprintln(w, "Create a new user...")
+	// Otherwise add a confirmation flash message to the session confirming that
+	// their signup worked and asking them to log in.
+	app.session.Put(r, "flash", "Your signup was successful. Please log in.")
+
+	// And redirect the user to the login page.
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -2,8 +2,12 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/unknowntpo/snippetbox/pkg/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserModel struct {
@@ -12,6 +16,25 @@ type UserModel struct {
 
 // We'll use the Insert method to add a new record to the users table.
 func (m *UserModel) Insert(name, email, password string) error {
+	// Create a bcrypt hash of the plain-text password.
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+    VALUES(?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		var mySQLError *mysql.MySQLError
+		if errors.As(err, &mySQLError) {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+		return err
+	}
 	return nil
 }
 
