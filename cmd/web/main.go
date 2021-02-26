1b5304b80f063cb0a739b52ecb14e3bd01bcadfd
Chang Chen Chien <e850506@gmail.com>
2021-02-26 09:36:00 +0800

    Chapter 13.4. Mocking Dependencies
    We’ve updated the application struct so that instead of the snippets and users fields having the concrete types *mysql.SnippetModel and *mysql.UserModel they are interfaces instead.
    
    So long as an object has the necessary methods to satisfy the interface,
    we can use them in our application struct. Both our ‘real’ database
    models (under the pkg/models/mysql package) and mock database models
    (under the pkg/models/mock package) satisfy the interfaces, so we can
    now use them interchangeably.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/unknowntpo/snippetbox/pkg/models"
 	"github.com/unknowntpo/snippetbox/pkg/models/mysql"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -24,12 +25,20 @@ const contextKeyIsAuthenticated = contextKey("isAuthenticated")
 // web application. For now we'll only include fields for the two custom loggers, but
 // we'll add more to it as the build progresses.
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	session       *sessions.Session
-	snippets      *mysql.SnippetModel
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	session  *sessions.Session
+	snippets interface {
+		Insert(string, string, string) (int, error)
+		Get(int) (*models.Snippet, error)
+		Latest() ([]*models.Snippet, error)
+	}
 	templateCache map[string]*template.Template
-	users         *mysql.UserModel
+	users         interface {
+		Insert(string, string, string) error
+		Authenticate(string, string) (int, error)
+		Get(int) (*models.User, error)
+	}
 }
 
 func main() {
