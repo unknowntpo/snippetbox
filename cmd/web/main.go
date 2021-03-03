7b8770c68715f52d8dce5863b8be9703b47c5d06
Chang Chen Chien <e850506@gmail.com>
2021-03-03 09:48:14 +0800

    Chapter 2.7. Project Structure and Organization - Refactor: seperate handlers
    Move handlers into ./cmd/web/handlers.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,40 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	// restrict the root url pattern
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from Snippetbox"))
-}
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
-		return
-	}
-	w.Write([]byte("Create a new snippet..."))
-}
 
 func main() {
 	mux := http.NewServeMux()
