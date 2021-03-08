a871f23bb08566ab656d841dbb328429bd6dc305
Chang Chen Chien <e850506@gmail.com>
2021-03-08 14:22:50 +0800

    Chapter 2.6. URL Query Strings

--- main.go
+++ main.go
@@ -18,7 +18,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Add a showSnippet handler function.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 // Add a createSnippet handler function.
