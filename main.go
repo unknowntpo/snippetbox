2a525927ecd47dca82e131c267dce3b23276bf5b
Chang Chen Chien <e850506@gmail.com>
2021-03-08 14:22:01 +0800

    Chapter 2.5. Customizing HTTP Headers

--- main.go
+++ main.go
@@ -23,6 +23,11 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 
 // Add a createSnippet handler function.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
